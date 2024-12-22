37f7091ca677e8147accb9020d617920b653fa49
elad425 <elad425@gmail.com>
2024-12-22 19:06:13 +0200

    improve UI

--- app/src/main/java/com/example/gasmeterreader/activities/ReadingActivity.java
+++ app/src/main/java/com/example/gasmeterreader/activities/ReadingActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -55,7 +56,10 @@ public class ReadingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.transparent));
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
         setContentView(R.layout.activity_reading);
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
         getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
